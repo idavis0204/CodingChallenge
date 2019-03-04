f1d3f62f51196a4a88a2125d8ec08ed7aa48ccf5
Iain <idavis0204@gmail.com>
2019-03-04 20:32:34 +0200

    Part 2 Complete

--- FrontEnd/Part2.js
+++ FrontEnd/Part2.js
@@ -49,3 +49,39 @@ function GetSumOfHighRankingDestinationRankings()
     var highRankingDestinations = GetHighRankingDestinations();
     return highRankingDestinations.reduce(add, 0); 
 }
+
+// SECTION 2
+
+// 1. Why will the function call return true?
+//
+// The function will return true as javascript is a weakly typed language and thus does not enforces correct typing.
+
+// 2. How could one change this function so that data types are checked as well as values?
+
+// Using the typeof operator to determine the type of each variable 
+
+// e.g: 
+
+function compareIt(num1, num2)
+{
+    return (typeof (num1) == typeof (num2)) && (num1 == num2);
+}
+
+// You could also consider using a tool such as TypeScript to write javascript in a similar manner to a strongly typed language.
+
+// SECTION 3
+
+// 1. Why will the function be parsed correctly?
+
+// Javascript does not enforce unique variable names. Thus when a new variable is declared with the same name as an existing variable, the new variable will overwrite the existing variable.
+
+// 2. How could you introduce a stricter syntax to variable/function declaration and avoid this behaviour?
+
+// You could add a "use strict" statement to the start of the file or function to instruct the browser to use Strict mode. Once enabled, strict mode will enforce that variable names need
+// to be unique within the scope defined by where the "use strict" statement. e.g. Entire files if declared at the top of the file. 
+
+function legal(x) {
+    "use strict";
+    console.log(x);
+    var x;
+}
